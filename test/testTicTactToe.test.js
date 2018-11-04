f0ba002376c9f5bae1c5932d897ebe17923b0713
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2018-11-04 16:33:42 +0100

    add new test for testing game id

--- test/testTicTactToe.test.js
+++ test/testTicTactToe.test.js
@@ -1,20 +1,38 @@
 const TicTacToe = artifacts.require("./TicTacToe.sol");
 
-contract.only("TicTacToe", ([player1, player2]) => {
+contract("TicTacToe", ([player1, player2]) => {
   let ticTacToe;
 
   beforeEach(async () => {
     ticTacToe = await TicTacToe.new();
   });
 
-describe("#newGame", () => {
-    it("creates a new game with player1 set to msg sender", async () => {
-      await ticTacToe.newGame(player2, { from: player1 })
+  describe("#newGame", () => {
+      it("creates a new game with player1 set to msg sender", async () => {
+        await ticTacToe.newGame(player2, { from: player1 })
 
-      const gameId = await ticTacToe.gameCounter()
-      const players = await ticTacToe.getPlayers(gameId.toNumber())
+        const gameId = await ticTacToe.gameCounter()
+        const players = await ticTacToe.getPlayers(gameId.toNumber())
 
-      assert.equal(players[0], player1, 'first player is the msg sender')
-    });
+        assert.equal(players[0], player1, 'first player is the msg sender')
+      });
+
+      it("gives each game a new id", async () => {
+        // game 1
+        await ticTacToe.newGame(player2, { from: player1 })
+        const gameId = await ticTacToe.gameCounter()
+        let players = await ticTacToe.getPlayers(gameId.toNumber())
+
+        assert.equal(gameId, 1, "first game");
+        assert.equal(players[0], player1, "first player is the msg sender");
+
+        // game 2
+        await ticTacToe.newGame(player1, { from: player2 })
+        const gameId2 = await ticTacToe.gameCounter()
+        players = await ticTacToe.getPlayers(gameId2.toNumber());
+
+        assert.equal(gameId2, 2, "first game");
+        assert.equal(players[0], player2, "first player is the msg sender");
+      });
   });
 });
